730117ebabfe9255bb4ae3c7e2c7faa0097738ae
Diego <drodriguez18@ucol.mx>
2021-10-28 17:43:37 -0500

    mobile-home

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/shared/logo.svg';
+import IconMenu from '../assets/shared/icon-hamburger.svg';
+import closeMenu from '../assets/shared/icon-close.svg';
+import { SidebarData} from '../data/SidebarData';
+import '../styles/navbar.css';
+
+const Navbar = () => {
+
+    const [ sidebar, setSidebar ] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
+    return (
+        <>
+            <div className="navbar">
+                <Link to="/" className="logo">
+                    <img src={logo}  alt={'logo'}/>
+                </Link>
+
+                <Link to="/" className="menu-bars">
+                    <img src={IconMenu} onClick={showSidebar} alt={'menu-bars'}/>
+                </Link>
+            </div>
+
+            <div className='menu-content'>
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items'>
+                        <li className='navbar-toggle'>
+                            <Link to='#'>
+                                <img src={closeMenu} onClick={showSidebar} alt={'menu-bars'}/>
+                            </Link>
+                        </li>
+                        {
+                            SidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path} onClick={showSidebar} className='link-menu'>
+                                            {item.num}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
---
+++ src/data/SidebarData.js
@@ -0,0 +1,27 @@
+
+export const SidebarData = [
+    {
+        num: '00',
+        title: 'HOME',
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        num: '01',
+        title: 'DESTINATION',
+        path: '/destination',
+        cName: 'nav-text',
+    },
+    {
+        num:'02',
+        title: 'CREW',
+        path: '/destination',
+        cName: 'nav-text',
+    },
+    {
+        num: '03',
+        title: 'TECHNOLOGY',
+        path: '/technology',
+        cName: 'nav-text'
+    }
+]
